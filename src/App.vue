6ecd20b733d34c7f73a624e77501adc36462684b
abc02 <abcgg02@gmail.com>
2018-04-16 16:17:43 +0800

    init vue-amap demo run

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class="amap-wrapper">
+      <!-- <el-amap class="amap-box" :vid="'amap-vue'"></el-amap> -->
+      <el-amap ref="map" vid="amapDemo" :amap-manager="amapManager" :center="center" :zoom="zoom" :plugin="plugin" :events="events" class="amap-demo">
+      </el-amap>
+
+      <!-- <div class="toolbar">
+        <button @click="getMap()">get map</button>
+      </div> -->
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import { AMapManager } from 'vue-amap'
+let amapManager = new AMapManager()
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      amapManager,
+      zoom: 12,
+      center: [121.59996, 31.197646],
+      events: {
+        init: (o) => {
+          console.log(o.getCenter())
+          console.log(this.$refs.map.$$getInstance())
+          o.getCity(result => {
+            console.log(result)
+          })
+        },
+        'moveend': () => {
+        },
+        'zoomchange': () => {
+        },
+        'click': (e) => {
+          alert('map clicked')
+        }
+      },
+      plugin: ['ToolBar', {
+        pName: 'MapType',
+        defaultType: 0,
+        events: {
+          init (o) {
+            console.log(o)
+          }
+        }
+      }]
+    }
+  },
+  methods: {
+    getMap () {
+      // amap vue component
+      console.log(amapManager._componentMap)
+      // gaode map instance
+      console.log(amapManager._map)
+    }
   }
 }
 </script>
 
 <style>
+html,body,div{
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+.amap-wrapper {
+  height: 100%;
+  height: 100vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAMap from 'vue-amap'
 
+Vue.use(VueAMap)
+VueAMap.initAMapApiLoader({
+  key: '7204bfff049e6370b935f9ccbf3bf61e',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  // 默认高德 sdk 版本为 1.4.4
+  v: '1.4.4'
+})
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
